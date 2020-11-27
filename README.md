08a0b9fac426f9c17cb9731d8a4d26e2aba87efa
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2020-11-27 14:13:33 -0800

    Analyze all files in input folder

--- README.md
+++ README.md
@@ -4,13 +4,12 @@
 ## Usage
 1. Clone the project locally
 2. In the project root, create an `input` folder and an `output` folder
-3. Place a `.txt` file containing sentences/paragraphs (that you'd like to use emotion recognition on) into the `input` directory
+3. Place `.txt` file(s) containing sentences/paragraphs (that you'd like to use emotion recognition on) into the `input` directory
     - Note: Each sentence should be delimited by `.` or `!` (as in regular English)
-4. Open `bert_predict.py` and change the `input_text` variable contents to be the name of your `.txt` file
-5. In a terminal, `cd` into the project root
-6. Run `pip install -r requirements.txt`
-7. Run `python bert_predict.py` to use the pre-trained BERT NLP model to recognize emotions in each sentence
-8. View the output `.csv` files with the recognized emotions (per sentence) in the `output` directory
+4. In a terminal, `cd` into the project root
+5. Run `pip install -r requirements.txt`
+6. Run `python bert_predict.py` to use the pre-trained BERT NLP model to recognize emotions in each sentence
+7. View the output `.csv` files with the recognized emotions (per sentence) in the `output` directory
 
 ## Train your own model
 If you'd like to train your own model, see the `bert_init.py` file.
--- bert_predict.py
+++ bert_predict.py
@@ -13,10 +13,8 @@ import ktrain
 from ktrain import text
 
 ################################################################################
-########### Change these depending on what you name the transcripts ############
-input_text = 'transcript.txt'
-input_path = os.path.join('input',input_text)
-output_name = input_text.split('.')[0]
+########## Change this depending on where your recordings are located ##########
+rec_dir = 'input/'
 ################################################################################
 
 # #### Testing with other inputs
@@ -24,33 +22,51 @@ predictor = ktrain.load_predictor('models/bert_model')
 predictor.get_classes()
 # The classes are 'joy', 'sadness', 'fear', 'anger', 'neutral'
 
-# Read in the entire text
-f = open(input_path, 'r')
-messages = f.read()
-f.close()
+def getEmotionFromText(input_path):
+    # Read in the entire text
+    f = open(input_path, 'r')
+    messages = f.read()
+    f.close()
 
-messages_list = re.split('[\.!]', messages)
-sentiments = []
-i = 1
-iter_percent = 0 # for printing percent done
-for msg in messages_list:
-    # print(f'predicting for sentence: "{msg}"')
-    prediction = predictor.predict(msg)
-    # print('predicted: {}\n'.format(prediction))
-    sentiments.append(prediction)
+    messages_list = re.split('[\.!]', messages)
+    sentiments = []
+    i = 1
+    iter_percent = 0 # for printing percent done
+    for msg in messages_list:
+        # print(f'predicting for sentence: "{msg}"')
+        prediction = predictor.predict(msg)
+        # print('predicted: {}\n'.format(prediction))
+        sentiments.append(prediction)
 
-    # every so often, show percent done
-    percent_done = i/len(messages_list)*100
-    if (percent_done > iter_percent):
-        print('current message index: %.0f' % i)
-        print('percent done: %.1f%%' % percent_done)
-        iter_percent += 20
-    i += 1
+        # every so often, show percent done
+        percent_done = i/len(messages_list)*100
+        if (percent_done > iter_percent):
+            print('current message index: %.0f' % i)
+            print('percent done: %.1f%%' % percent_done)
+            iter_percent += 20
+        i += 1
 
-# Export predicted emotions to .csv format
-df = pd.DataFrame({'EMOTION': sentiments, 'SENTENCE': messages_list})
-df.to_csv(os.path.join('output', output_name + '_nlp_er.csv'), sep=',', index=False)
+    # Export predicted emotions to .csv format
+    df = pd.DataFrame({'EMOTION': sentiments, 'SENTENCE': messages_list})
+    df.to_csv(os.path.join('output', 'nlp_emotion_' + output_name + '.csv'), sep=',', index=False)
 
-print('🎉 Done! 🎉')
-print('See the output file:')
-print('output/' + output_name + '_nlp_er.csv')
+    print('🎉 Done! 🎉')
+    print('See the output file:')
+    print('output/' + 'nlp_emotion_' + output_name + '.csv')
+
+if __name__ == '__main__':
+    # Loop through specific files and analyze their text
+    files_in_dir = [f for f in os.listdir(rec_dir) if os.path.isfile(os.path.join(rec_dir, f))]
+    i = 0
+    for f in files_in_dir:
+        if f.split('.')[1] == 'txt':
+            input_path = os.path.join(rec_dir,f)
+            output_name = f.split('.')[0]
+
+            print(f'Reading from {input_path}')
+
+            getEmotionFromText(input_path)
+
+        i += 1
+        print(f"""Number of files to go: {len(files_in_dir) - i}
+            Percent files done: {i/len(files_in_dir)*100}\n""")
